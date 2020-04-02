2406d9c0dd7ad9f78714a68f98acf5226c717f23
lienapaeps <lienapaeps@users.noreply.github.com>
2020-04-02 20:19:06 +0200

    Initial commit

---
+++ src/ui/controller/Occurences.java
@@ -0,0 +1,32 @@
+package ui.controller;
+
+import domain.model.LetterCounter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Occurences")
+public class Occurences extends HttpServlet {
+
+    public LetterCounter test = new LetterCounter();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        char letter = request.getParameter("letter").charAt(0);
+        String woord = request.getParameter("woord");
+
+        int aantalLetters = LetterCounter.countLetters(woord, letter);
+
+        request.setAttribute("aantalKeer", aantalLetters);
+
+        request.getRequestDispatcher("result.jsp").forward(request, response);
+    }
+}
